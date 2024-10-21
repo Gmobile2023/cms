fa5c1f9f4049833595d2a8a2d2549298b7acf912
buithecuong <buithecuong6789@gmail.com>
2024-10-21 09:43:50 +0700

    Add role Claim

--- ServiceStackBaseCms.ServiceModel/UserClaim.cs
+++ ServiceStackBaseCms.ServiceModel/UserClaim.cs
@@ -35,11 +35,6 @@ public class CreateUserClaimRequest
 public class UpdateUserClaimRequest
 {
     public int Id { get; set; }
-
-    public string UserId { get; set; }
-
-    public string? ClaimType { get; set; }
-
     public string? ClaimValue { get; set; }
 }
 
